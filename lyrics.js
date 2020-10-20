ce4bdf06e34ad3e4275fbefce3ead686410ea442
Fuad Hasan <fuad.hasan.1997@gmail.com>
2020-10-20 20:46:38 +0600

    Api Installed

--- lyrics.js
+++ lyrics.js
@@ -1,8 +1,14 @@
-const apiUrl = 'https://api.lyrics.ovh/suggest/';
-const 
 
-fetch('https://api.lyrics.ovh/suggest/')
-.then(res => res.json())
-.then(data =>  {
+const apiUrl = 'https://api.lyrics.ovh/suggest/';
+const searchButton = document.getElementById('search-button');
+searchButton.addEventListener('click', () =>{
+    const searchWord = document.getElementById('search').value;
+    //console.log(searchWord);
+
+    fetch(`${apiUrl}${searchWord}`)
+    .then(res => res.json())
+    .then(data =>  {
     console.log(data);
 })
+})
+
