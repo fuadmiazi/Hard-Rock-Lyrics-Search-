7d863ca62e7169853c7a00bbd02e06c0ac938f00
Fuad Hasan <fuad.hasan.1997@gmail.com>
2020-10-21 22:29:02 +0600

    Load Search Result

--- lyrics.js
+++ lyrics.js
@@ -7,8 +7,28 @@ searchButton.addEventListener('click', () =>{
 
     fetch(`${apiUrl}${searchWord}`)
     .then(res => res.json())
-    .then(data =>  {
-    console.log(data);
-})
-})
+    .then(datas =>  {
+    const songList = datas.data;
+    console.log(songList);
+    var songArray = songList.length;
+    var paraParent, songPara, songName;
+    
+    // songName.innerHTML = "";
+    // albumName.innerHTML = "";
 
+    for(i=0; i<songArray; i++){
+        var paraParent = document.getElementById("search-result");
+        var songPara = document.createElement("p");
+        paraParent.appendChild(songPara);
+        var songName = document.createElement("strong");
+        songPara.appendChild(songName);
+        var middleText = document.createTextNode(" Album By ");
+        songPara.appendChild(middleText);
+        var albumName = document.createElement("strong");
+        songPara.appendChild(albumName);
+        songName.innerHTML = songList[i].title;
+        albumName.innerHTML = songList[i].album.title;
+    }
+    
+})
+})
